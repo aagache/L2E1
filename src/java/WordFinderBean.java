6a743ebde540ebc0dca1df1246cf10f67ebc115b
Andrei Agache <andrei_hks@yahoo.com>
2016-10-31 21:57:15 +0200

    Updated the jsp files and the interaction in-between

---
+++ src/java/WordFinderBean.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class WordFinderBean implements java.io.Serializable {
+    String letters;
+    List<String> words = new ArrayList<String>();
+
+    public WordFinderBean(String letters, List<String> words) {
+        this.letters = letters;
+        this.words = words;
+    }
+
+    public String getLetters() {
+        return letters;
+    }
+
+    public void setLetters(String theLetters) {
+        letters = theLetters;
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public void setWords(List<String> theWords) {
+        words = theWords;
+    }
+}
